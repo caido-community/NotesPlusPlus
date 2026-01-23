10d667e55cfa3713964cdbbf5387de808e3c5a86
Emile Fugulin <code@efugulin.com>
2026-01-23 17:16:44 -0500

    Fix release

--- caido.config.ts
+++ caido.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   id,
   name: "Notes++",
   description: "Create and edit notes in Caido",
-  version: "2.3.0",
+  version: "2.3.1",
   author: {
     name: "_StaticFlow_",
     email: "tanner.barnes@kashx.io",
